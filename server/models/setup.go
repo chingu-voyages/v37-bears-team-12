9adf56835266ffabdb368d32756a498a8a04949d
Carlos Martinez <curlosmart@gmail.com>
2022-03-01 08:43:15 -0500

    setup database connection

--- server/models/setup.go
+++ server/models/setup.go
@@ -1,14 +1,21 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open("sqlite3", "test.db")
+	godotenv.Load()
+	dsn := os.Getenv("DB_CONNECTION_STRING")
+	fmt.Println(dsn)
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database!")
