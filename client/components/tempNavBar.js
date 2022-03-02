9dfe260430ba0de9767701968651fff3ec63b7c2
Mateo <mtaycruz@gmail.com>
2022-03-02 14:35:43 -0500

    added spacing between links

--- client/components/tempNavBar.js
+++ client/components/tempNavBar.js
@@ -4,21 +4,21 @@ import { HomeIcon, DocumentIcon, DocumentAddIcon, InformationCircleIcon, LogoutI
 export default function TempNavBar() {
     return (
         <aside className="border-2 border-black md:h-screen md:w-2/12 bg-[#A49EA2]">
-            <div className="ml-4 mt-6 text-white">
-                <h2 className="text-3xl mb-4">Whatever Note</h2>
-                <h3 className="mb-4 text-2xl">User Name</h3>
+            <div className="text-white">
+                <h2 className="text-3xl mb-4 ml-4 mt-6">Whatever Note</h2>
+                <h3 className="mb-4 text-2xl ml-4 ">User Name</h3>
                 <ul className="flex flex-col">
                     <Link href="/dashboard">
-                        <a className="underline mb-3 text-lg flex"><HomeIcon className="h-6 w-6 mr-2"/> Dashboard</a>
+                        <a className="underline pl-4 mb-3 text-lg h-11 flex items-center hover:bg-black hover:opacity-25"><HomeIcon className="h-6 w-6 mr-2"/> Dashboard</a>
                     </Link>
                     <Link href="/notes">
-                        <a className="underline mb-3 text-lg flex"><DocumentIcon className="h-6 w-6 mr-2"/>Notes</a>
+                        <a className="underline pl-4 mb-3 text-lg h-11 flex items-center hover:bg-black hover:opacity-25"><DocumentIcon className="h-6 w-6 mr-2"/>Notes</a>
                     </Link>
-                    <li className="mb-3 text-lg flex"><DocumentAddIcon className="h-6 w-6 mr-2"/>Add a Note</li>
+                    <li className="pl-4 mb-3 text-lg h-10 flex items-center hover:bg-black hover:opacity-25"><DocumentAddIcon className="h-6 w-6 mr-2"/>Add a Note</li>
                     <Link href="/about">
-                        <a className="underline mb-3 text-lg flex"><InformationCircleIcon className="h-6 w-6 mr-2 "/>About</a>
+                        <a className="underline pl-4 mb-3 text-lg h-11 flex items-center hover:bg-black hover:opacity-25"><InformationCircleIcon className="h-6 w-6 mr-2 "/>About</a>
                     </Link>
-                    <li className="mb-3 text-lg flex"><LogoutIcon className="h-6 w-6 mr-2 mt-1"/>Sign Out</li>
+                    <li className="pl-4 mb-3 text-lg h-11 flex items-center hover:bg-black hover:opacity-25"><LogoutIcon className="h-6 w-6 mr-2 mt-1"/>Sign Out</li>
                 </ul>
             </div>
         </aside>
