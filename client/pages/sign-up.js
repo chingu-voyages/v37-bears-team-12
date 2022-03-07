fcc9faef65df7ee1f152d55425f0a6a65d150ed8
Mateo <mtaycruz@gmail.com>
2022-03-07 12:17:14 -0500

    background and header added to signup page

--- client/pages/sign-up.js
+++ client/pages/sign-up.js
@@ -2,11 +2,28 @@ import Link from "next/link";
 
 export default function signUp() {
     return (
-        <>
-            <h1>This is the sign up page</h1>
-            <Link href="/">
-                <a className="text-blue-900 underline">Return home</a>
-            </Link>
-        </>
+        <div className="h-screen bg-cover bg-[url('/images/coffee-notebook.jpg')] overflow-auto">
+            <div className="flex flex-col h-screen">
+                <header className="flex justify-between items-center">
+                    <span className="px-5 text-xl">LOGO</span>
+                    <nav className="text-sm md:text-xl py-4">
+                        <ul className="flex">
+                            <Link href="/login">
+                                <a className="bg-white rounded-xl px-2 py-1 mx-3 hover:text-white hover:bg-transparent">Login</a>
+                            </Link>
+                            <Link href="/sign-up">
+                                <a className="bg-white rounded-xl px-2 py-1 mx-3 hover:text-white hover:bg-transparent">Sign up</a>
+                            </Link>
+                        </ul>
+                    </nav>
+                </header>
+                <main>
+                    
+                </main>
+            
+            
+            </div>
+        
+        </div>
     );
 }
