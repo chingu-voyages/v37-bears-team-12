9c5b2adf583e88ab23a0e18a900f6b8497991d80
zxcv3 <zxcv34332@gmail.com>
2022-03-10 11:32:22 +0900

    fix: fix port path

--- server/middleware/jwt-auth.go
+++ server/middleware/jwt-auth.go
@@ -4,9 +4,8 @@ import (
 	"log"
 	"net/http"
 
-	"notes-app/helper"
-	"notes-app/service"
-
+	"github.com/chingu-voyages/v37-bears-team-12/helper"
+	"github.com/chingu-voyages/v37-bears-team-12/service"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
