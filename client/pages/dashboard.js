c1033fd9a00195c578283817e35b82156569781f
Daryl Nauman <darylnauman@gmail.com>
2022-03-16 11:32:26 -0400

    Dashboard display the four most recent notes, adjusted note cards animation including styling

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -5,7 +5,24 @@ import TempNavBar from "../components/tempNavBar";
 export default function dashboard({data}) {
     
     const notes = data;
-    console.log(data);
+    let sortedNotes = [];
+
+    // if more than one note, sort notes by created date with newest notes at the start of the array
+    if (notes.length > 1) {
+        sortedNotes = notes.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    } else {
+        sortedNotes = notes;
+    }
+    
+    // create an array with only four notes to display on the dashboard
+    let recentNotes = [];
+    if (sortedNotes.length > 4) {
+        for (let i = 0; i < 4; i++) {
+            recentNotes[i] = sortedNotes[i];
+        }
+    } else {
+        recentNotes = sortedNotes;
+    }
 
     return (
         <div className="flex flex-col md:flex-row">
@@ -15,12 +32,12 @@ export default function dashboard({data}) {
                 <GreetingDate />
 
                 <h1 className="w-11/12 mx-auto text-center text-black bg-white opacity-75 rounded-3xl text-2xl p-2">
-                    Your Recent Notes
+                    Recent Notes
                 </h1>
 
                 <section className="bg-white w-11/12 opacity-75 rounded-3xl mx-auto my-8">
                     <div className="flex flex-wrap justify-around">
-                    {notes.map(note => (
+                    {recentNotes.map(note => (
                         <NotesCard
                             key={note.id}
                             id={note.id}
@@ -31,12 +48,6 @@ export default function dashboard({data}) {
                     ))}
                     </div>
                 </section>
-
-                {/* <section className="bg-white h-fit w-11/12 opacity-75 rounded-3xl mx-auto mt-10">
-                    <h1 className="text-black text-center text-2xl py-40 mb-4">
-                        Create a Note [insert WYSIWYG here]
-                    </h1>
-                </section> */}
             </main>
         </div>
     );
