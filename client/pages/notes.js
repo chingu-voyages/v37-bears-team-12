542eb64284dd66ee63537f4af8294c98a7a50b7d
Daryl Nauman <darylnauman@gmail.com>
2022-03-23 14:33:59 -0400

    Add ability to filter notes by subject

--- client/pages/notes.js
+++ client/pages/notes.js
@@ -1,10 +1,38 @@
 import GreetingDate from "../components/greetingDate";
 import NotesCard from "../components/notesCard";
 import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
 
-export default function notes({ data }) {
-    const notes = data;
+export default function notes() {
     
+    const [notes, setNotes] = useState([]);
+    const [subject, setSubject] = useState('*');
+
+    const handleSubjectChange = (e) => {
+        setSubject(e.target.value);
+    };
+
+    useEffect(async () => {
+        let url;
+        if (subject === '*') {
+            url = `https://bwnxxxhdcgewlvmpwdkl.supabase.co/rest/v1/notes`
+        } else {
+            url = `https://bwnxxxhdcgewlvmpwdkl.supabase.co/rest/v1/notes?subject=eq.${subject}&select=*`
+        }
+        
+        const res = await fetch(
+            url,
+            {
+                method: "GET",
+                headers: {
+                    apiKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+                },
+            }
+        );
+        const data = await res.json();        
+        setNotes(data);
+    },[subject]);
+
     return (
         <div className="flex flex-col md:flex-row">
             <NavBar />
@@ -12,12 +40,29 @@ export default function notes({ data }) {
             <main className="md:w-10/12 bg-cover bg-[url('/images/coffee-notebook.jpg')]">
                 <GreetingDate />
 
-                <h1 className="w-11/12 mx-auto text-center text-black bg-white opacity-75 rounded-3xl text-2xl p-2">
-                    Notes
-                </h1>
+                <section className="w-11/12 mx-auto text-black bg-white opacity-75 rounded-3xl text-2xl p-2">
+                    <h1 className="text-center">Notes</h1>
+
+                    <label className="text-base block">
+                        Filter by subject:
+                        <select
+                            className="text-left"
+                            id="subjects"
+                            name="subjects"
+                            onChange={handleSubjectChange}
+                        >
+                            <option value="*">All</option>
+                            <option value="Biology">Biology</option>
+                            <option value="Calculus">Calculus</option>
+                            <option value="History">History</option>
+                            <option value="Physics">Physics</option>
+                            <option value="English">English</option>
+                        </select>
+                    </label>
+                </section>
+                
                 <section className="bg-white w-11/12 opacity-75 rounded-3xl mx-auto mt-10 p-3">
-                   
-                    {notes.map(note => (
+                    {notes.map((note) => (
                         <NotesCard
                             key={note.id}
                             id={note.id}
@@ -27,26 +72,8 @@ export default function notes({ data }) {
                             content={note.content}
                         />
                     ))}
-
                 </section>
             </main>
         </div>
     );
-}
-
-export async function getServerSideProps(context) {
-    // Fetch data from external API
-    const res = await fetch(
-        `https://bwnxxxhdcgewlvmpwdkl.supabase.co/rest/v1/notes`,
-        {
-            method: "GET",
-            headers: {
-                apiKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-            },
-        }
-    );
-    const data = await res.json();
-
-    // Pass data to the page via props
-    return { props: { data } };
 }
