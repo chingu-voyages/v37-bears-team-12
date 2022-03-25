968b530b8aa9a3203ad44ee28cbc3d94711370cd
Mateo <mtaycruz@gmail.com>
2022-03-25 14:22:11 -0400

    changed home page responsiveness on about and developer card

--- client/pages/index.js
+++ client/pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
                             <div className="items-end w-1/2 mb-10 ml-16">
                                 <h3 className="text-3xl">Features</h3>
                             </div>
-                            <ul className="mx-16 mb-16 list-disc leading-9">
+                            <ul className="mx-16 mb-16 list-disc leading-9 m-10 mt-0 w-1/2">
                                 <li>Users are able to create and edit notes using a powerful rich text editor</li>
                                 <li>Users are able to read and delete notes</li>
                                 <li>Users can filter notes by subject</li>
@@ -63,48 +63,48 @@ export default function Home() {
                     <div className="w-8/12 h-fit flex items-center flex-col bg-slate-50/50 rounded-lg mt-32 mb-16">
                             <h1 className="m-16 md:text-6xl">Developers</h1>
                             <div className="card-group flex pb-10 flex-wrap justify-center">
-                                <div className="card w-1/4 flex flex-col items-center text-center m-2">
+                                <div className="card w-60 flex flex-col items-center text-center mx-6 mb-6">
                                     <div className="shape">
                                         <div className="circle">
                                             <Image className="rounded-full" src={carlos}/>
                                         </div>
                                     </div>
                                     <div className="card-body">
-                                        <h3 className="m-4 md:text-2xl">Developer</h3>
-                                        <p>Donec semper dapibus commodo. Curabitur ac leo mauris. Curabitur eleifend, lorem vel suscipit auctor, velit dui ultricies est, eu ornare lorem risus et turpis. .</p>
+                                        <h3 className="m-4 md:text-2xl">Carlos Martinez</h3>
+                                        <p>Software Developer with a focus on building responsive websites and solving problems.</p>
                                     </div>
                                 </div>
-                                <div className="card w-1/4 flex flex-col items-center text-center m-2 mx-6">
+                                <div className="card w-60 flex flex-col items-center text-center mx-6 mb-6">
                                     <div className="shape">
                                         <div className="circle">
                                             <Image className="rounded-full" src={daryl}/>
                                         </div>
                                     </div>
                                     <div className="card-body">
-                                        <h3 className="m-4 md:text-2xl">Developer</h3>
-                                        <p>Donec semper dapibus commodo. Curabitur ac leo mauris. Curabitur eleifend, lorem vel suscipit auctor, velit dui ultricies est, eu ornare lorem risus et turpis. .</p>
+                                        <h3 className="m-4 md:text-2xl">Daryl Nauman</h3>
+                                        <p>Full-stack web developer with an Executive MBA and leadership experience at post-secondary institutions</p>
                                     </div>
                                 </div>
-                                <div className="card w-1/4 flex flex-col items-center text-center m-2">
+                                <div className="card w-60 flex flex-col items-center text-center mx-6 mb-6">
                                     <div className="shape">
                                         <div className="circle">
                                             <Image className="rounded-full" src={z}/>
                                         </div>
                                     </div>
                                     <div className="card-body">
-                                        <h3 className="m-4 md:text-2xl">Developer</h3>
-                                        <p>Donec semper dapibus commodo. Curabitur ac leo mauris. Curabitur eleifend, lorem vel suscipit auctor, velit dui ultricies est, eu ornare lorem risus et turpis. .</p>
+                                        <h3 className="m-4 md:text-2xl">Z</h3>
+                                        <p>Web Developer</p>
                                     </div>
                                 </div>
-                                <div className="card  w-1/4 flex flex-col items-center text-center m-2">
+                                <div className="card  w-60 flex flex-col items-center text-center mx-6 mb-6">
                                     <div className="shape">
                                         <div className="circle">
                                             <Image className="rounded-full" src={mateo}/>
                                         </div>
                                     </div>
                                     <div className="card-body">
-                                        <h3 className="m-4 md:text-2xl">Developer</h3>
-                                        <p>Donec semper dapibus commodo. Curabitur ac leo mauris. Curabitur eleifend, lorem vel suscipit auctor, velit dui ultricies est, eu ornare lorem risus et turpis. .</p>
+                                        <h3 className="m-4 md:text-2xl">Mateo Cruz</h3>
+                                        <p>Front End Web Developer with a background in customer service</p>
                                     </div>
                                 </div>
                             </div>
